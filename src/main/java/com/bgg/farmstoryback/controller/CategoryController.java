0f059d24095e828424e43c975763d8266be2b18e
this88mild7 <this88mild7@gmail.com>
2013-08-07 17:58:42 +0900

    전시 관리 구현중

--- src/main/java/com/bgg/farmstoryback/controller/CategoryController.java
+++ src/main/java/com/bgg/farmstoryback/controller/CategoryController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.bgg.farmstoryback.service.CategoryService;
-import com.bgg.farmstoryback.service.UserService;
 
 @Controller
 public class CategoryController {
---
+++ src/main/java/com/bgg/farmstoryback/service/DispalyService.java
@@ -0,0 +1,18 @@
+package com.bgg.farmstoryback.service;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bgg.farmstoryback.dao.DisplayDao;
+import com.bgg.farmstoryback.dao.ViewDao;
+
+@Service
+public class DispalyService {
+
+		@Autowired
+		private DisplayDao displayDao;
+		
+}
