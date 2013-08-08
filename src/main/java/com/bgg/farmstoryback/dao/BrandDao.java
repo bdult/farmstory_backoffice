03f1a212b303d7f29379215b7490b96315fb706a
this88mild7 <this88mild7@gmail.com>
2013-08-08 17:37:34 +0900

    컨텐츠 관리 기능 추가 -- 삭제 수정은 제외

---
+++ src/main/java/com/bgg/farmstoryback/dao/BrandDao.java
@@ -0,0 +1,30 @@
+package com.bgg.farmstoryback.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import org.mybatis.spring.support.SqlSessionDaoSupport;
+import org.springframework.stereotype.Repository;
+
+import com.bgg.farmstoryback.dto.UserDto;
+
+
+@Repository
+public class BrandDao extends SqlSessionDaoSupport {
+
+	public List<Map> listByCateId(String cateId) {
+		return (List<Map>)getSqlSession().selectList("brandQuery.listByCateId", cateId);
+	}
+	
+	public List<Map> list() {
+		return (List<Map>)getSqlSession().selectList("brandQuery.list");
+	}
+
+	public void create(Map parameterMap) {
+		getSqlSession().insert("brandQuery.create", parameterMap);
+	}
+	
+	public void createCateBrand(Map parameterMap) {
+		getSqlSession().insert("brandQuery.createCateBrand", parameterMap);
+	}
+}
---
+++ src/main/java/com/bgg/farmstoryback/service/ContentsService.java
@@ -0,0 +1,81 @@
+package com.bgg.farmstoryback.service;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.bgg.farmstoryback.dao.ContentsDao;
+import com.bgg.farmstoryback.dao.UserDao;
+import com.bgg.farmstoryback.dto.UserDto;
+
+
+
+@Service
+public class ContentsService {
+	
+	private final String parentPath = "/var/lib/tomcat6/webapps/storyfarm/source/";
+
+	@Autowired
+	private ContentsDao contsDao;
+
+	public List<Map> list() {
+		return contsDao.list();
+	}
+	
+	public List<Map> listBySeriseId(String seriseId) {
+		return contsDao.listBySeriseId(seriseId);
+	}
+
+	
+	/**
+	 * 컨텐츠 생성
+	 * @param itemInfo
+	 */
+	public void createItem(Map<String, Object> itemInfo) {
+		
+		// item_id make
+		itemInfo.put("item_id", itemIdMake());
+		
+		// 원본 파일 경로 생성
+		itemInfo.put("src_path", makeFilePath((MultipartFile)itemInfo.get("file")));
+		contsDao.create(itemInfo);
+		
+	}
+
+	private String itemIdMake() {
+		UUID uid = UUID.randomUUID();
+		return uid.toString().replace("-", "");
+	}
+
+	
+	private String makeFilePath(MultipartFile file) {
+		try {
+			
+			//디렉토리 생성 
+			File desti = new File(parentPath);
+
+		  //해당 디렉토리의 존재여부를 확인
+			if(!desti.exists()){
+				desti.mkdirs(); 
+			}
+			String extenstion = file.getOriginalFilename().substring(file.getOriginalFilename().length()-3);
+			String fileName = UUID.randomUUID().toString().replace("-", "")+"."+extenstion;
+			byte fileData[] = file.getBytes();
+			FileOutputStream fos = new FileOutputStream(parentPath +fileName);
+			fos.write(fileData);
+			fos.close();
+			return fileName;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	
+}
