1c3502f77f18de6429f11d3ab1f21a93f88b866e
RNTS-LAPTOP <qotkwk@gmail.com>
2013-11-12 11:41:01 +0900

    Fixed contents search
    검색 조건 유지 변경 및 통계 수정

--- src/main/java/com/bgg/farmstoryback/controller/StatsController.java
+++ src/main/java/com/bgg/farmstoryback/controller/StatsController.java
@@ -58,10 +58,6 @@ public class StatsController {
 	public String view(Model model,  @RequestParam Map parameter) {
 		
 		logger.debug("My access token is {}", statsService.getAccessToken());
-		{//선차트용 데이터 가져오기
-			String dimension = "ga:date"; //year,month,week
-			model.addAttribute("lineChartData", statsService.getVisitor(dimension, dateUtil.add(-30), dateUtil.today()));
-		}
 		
 		{//평균정보 데이터 가져오기
 			
